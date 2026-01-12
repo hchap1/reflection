dc8aeeb07acd9131c7288d5f23d0752ffc53d6f1
hchap1 <hchap1@hotmail.com>
2026-01-12 19:18:10 +1000

    feat: error macro to generalise error types

---
+++ src/error/mod.rs
@@ -0,0 +1,30 @@
+macro_rules! error_enum {
+    (
+        $(#[$meta:meta])*
+        $vis:vis enum $name:ident {
+            $($variant:ident),* $(,)?
+        }
+    ) => {
+        $(#[$meta])*
+        #[derive(Debug, Clone)]
+        $vis enum $name {
+            $(
+                $variant($variant),
+            )*
+        }
+
+        $(
+            impl From<$variant> for $name {
+                fn from(e: $variant) -> Self {
+                    $name::$variant(e)
+                }
+            }
+        )*
+    };
+}
+
+error_enum! {
+    pub enum Error {
+
+    }
+}
