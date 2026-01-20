a9ed8acf4a9f0cf07ff41759de3dce8c84950cf4
hchap1 <hchap1@hotmail.com>
2026-01-20 12:29:54 +1000

    chore: TODO in main.rs

--- src/main.rs
+++ src/main.rs
@@ -11,3 +11,9 @@ mod frontend;
 fn main() {
 
 }
+
+// TODO Implement the rest of the SQL functions
+// TODO Create async function to retrieve all photos from an album and insert them all into the database
+// TODO Download scan function that checks every photo and makes sure they are downloaded
+// TODO Create wrapper around album scan function that does it for every cached album
+// TODO Begin work on frontend
