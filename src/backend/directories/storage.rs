52708c9eebebfd2e8119b3ac4ee6a6067e258c63
Harrison Chapman <hchap1@hotmail.com>
2026-06-16 11:17:46 +1000

    feat: create storage & singleton

--- src/backend/directories/storage.rs
+++ src/backend/directories/storage.rs
@@ -1,1 +1,58 @@
-// TODO Establish cross-platform data directory in an accessible and semantically sensible location to store database, images, and eventual config file
+use directories::ProjectDirs;
+
+use std::sync::OnceLock;
+use std::path::PathBuf;
+use std::fs::create_dir_all;
+
+use crate::error::{Error, Res};
+
+const QUALIFIER: &str = "com";
+const ORGANISATION: &str = "hchap1";
+const APPLICATION: &str = "reflection";
+
+pub static STORAGE: OnceLock<Storage> = OnceLock::new();
+
+pub struct Storage {
+
+    /// The root folder of the data directory
+    root: PathBuf,
+
+    /// The specific database file (.db)
+    database: PathBuf,
+
+    /// Root directory of photo storage
+    /// Actual photos should be <ROOT>/photos/<USER_ID>/<ALBUM_ID>/<PHOTO_ID>.ext
+    photos: PathBuf
+}
+
+impl Storage {
+
+    /// Find a suitable location and create filestructure
+    /// If it already exists, do nothing
+    pub fn initialise() -> Res<()> {
+        
+        let project_dirs = ProjectDirs::from(QUALIFIER, ORGANISATION, APPLICATION)
+            .ok_or(Error::FailedToFindStorageLocation)?;
+
+        let root = project_dirs
+            .data_dir()
+            .to_path_buf();
+
+        let database = root.join("database.db");
+        let photos = root.join("photos");
+
+        if !root.try_exists().map_err(|_| Error::CheckFileExistsError)? {
+            create_dir_all(&root)
+                .map_err(|_| Error::FailedToCreateDirectory(root.clone()))?;
+        }
+
+        if !photos.try_exists().map_err(|_| Error::CheckFileExistsError)? {
+            create_dir_all(&photos)
+                .map_err(|_| Error::FailedToCreateDirectory(photos.clone()))?;
+        }
+
+        STORAGE.get_or_init(|| Storage { root, database, photos });
+        Ok(())
+    }
+
+}
--- src/error.rs
+++ src/error.rs
@@ -1,8 +1,16 @@
+use std::path::PathBuf;
+
 pub type Res<T> = Result<T, Error>;
 
 #[derive(Debug, Clone, thiserror::Error)]
 pub enum Error {
 
     #[error("Failed to find storage location")]
-    FailedToFindStorageLocation
+    FailedToFindStorageLocation,
+
+    #[error("Could not check existance of root")]
+    CheckFileExistsError,
+
+    #[error("Could not create directory {:?}", .0)]
+    FailedToCreateDirectory(PathBuf)
 }
