0602f5c2d5982f6b5b30fd83bbbe2930be9d68f5
Harrison Chapman <hchap1@hotmail.com>
2026-06-30 11:06:18 +1000

    feat: verbump onedrive-albums, start skeleton tester in control.rs

--- src/bin/control.rs
+++ src/bin/control.rs
@@ -1,5 +1,16 @@
 // CONTROL APPLICATION
 
-fn main() {
+use lan_tcp::networking::node::Node;
+use onedrive_albums::authentication::oauth2;
+use reflection::{IDENTIFIER, PORT};
 
+#[tokio::main]
+async fn main() -> reflection::error::Res<()> {
+    let connection = Node::spawn_client(IDENTIFIER, PORT)
+        .await?;
+
+    let refresh_token =  oauth2::wrapper::acquire_refresh_token()
+        .await?;
+
+    Ok(())
 }
